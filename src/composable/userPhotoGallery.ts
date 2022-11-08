488d9966dd6cbea6080be911c7e82b70b0e998b6
Gabriel Felipe <gabriel.souza16@alunos.sc.senac.br>
2022-11-08 16:59:19 -0300

    aula dia 08/11

--- src/composable/userPhotoGallery.ts
+++ src/composable/userPhotoGallery.ts
@@ -1,7 +1,7 @@
 import { ref, onMounted, watch } from 'vue';
 import { Camera, CameraResultType, CameraSource, Photo } from "@capacitor/camera";
 import { Preferences } from "@capacitor/preferences";
-import { Filesystem, Directory } from "@capacitor/filesystem";
+import { Directory, Filesystem, } from "@capacitor/filesystem";
 
 export interface UserPhoto {
     filepath: string;
@@ -9,7 +9,42 @@ export interface UserPhoto {
 }
 
 export function userPhotoGallery() {
+    const PHOTO_STORAGE = 'photos';
     const photos = ref<UserPhoto[]>([]);
+
+    const cachePhotos = () => {
+        Preferences.set({
+            key: PHOTO_STORAGE,
+            value: JSON.stringify(photos.value),
+        });
+    };
+
+    const convertBlobToBase64 = (blob: Blob) =>
+        new Promise((resolve, reject) => {
+            const reader = new FileReader();
+            reader.onerror = reject;
+            reader.onload = () => {
+                resolve(reader.result);
+            };
+            reader.readAsDataURL(blob);
+        });
+
+    const savePicture = async (photo: Photo, fileName: string):
+        Promise<UserPhoto> => {
+        //let base64Data: string;
+        const response = await fetch(photo.webPath!);
+        const blob = await response.blob();
+        const base64Data = (await convertBlobToBase64(blob)) as string;
+        const savedFile = await Filesystem.writeFile({
+            path: fileName,
+            data: base64Data,
+            directory: Directory.Data,
+        });
+        return {
+            filepath: fileName,
+            webviewPath: photo.webPath,
+        };
+    };
 
     const takePhoto = async () => {
         const photo = await Camera.getPhoto({
@@ -19,12 +54,30 @@ export function userPhotoGallery() {
         });
 
         const fileName = new Date().getTime() + '.jpeg';
-        const savedFileImage = {
-            filepath: fileName,
-            webviewPath: photo.webPath,
-        };
+        const savedFileImage = await savePicture(photo, fileName);
+
+
+
         photos.value = [savedFileImage, ...photos.value];
     };
+
+    const loadSaved = async () => {
+        const photoList = await Preferences.get({ key: PHOTO_STORAGE });
+        const photosInPreferences = photoList.value ? JSON.parse(photoList.value) : [];
+
+        for (const photo of photosInPreferences) {
+            const file = await Filesystem.readFile({
+                path: photo.filepath,
+                directory: Directory.Data,
+            });
+            photo.webviewPath = `data:image/jpeg;base64,${file.data}`;
+        }
+
+        photos.value = photosInPreferences;
+    };
+
+    watch(photos, cachePhotos);
+    onMounted(loadSaved);
     return {
         photos,
         takePhoto,
--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -9,7 +9,7 @@
 
       <ion-grid>
         <ion-row>
-          <ion-col size="6" :key="photo" v-for="photo in photos">
+          <ion-col size="6" :Key="photo" v-for="photo in photos">
             <ion-img :src="photo.webviewPath"></ion-img>
           </ion-col>
         </ion-row>
