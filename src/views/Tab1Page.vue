543496727005e572987bf7f015fc34606f7468bd
Gabriel Felipe <gabriel.souza16@alunos.sc.senac.br>
2022-12-13 14:30:35 -0300

    salvando

--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -2,15 +2,15 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Pasta De Fotos</ion-title>
+        <ion-title>Galeria de fotos</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
 
       <ion-grid>
         <ion-row>
-          <ion-col size="6" :Key="photo" v-for="photo in photos">
-            <ion-img :src="photo.webviewPath"></ion-img>
+          <ion-col size="4" :Key="photo" v-for="photo in photos">
+            <ion-img @click="showActionSheet(photo)" :src="photo.webviewPath"></ion-img>
           </ion-col>
         </ion-row>
       </ion-grid>
@@ -23,49 +23,45 @@
     </ion-content>
   </ion-page>
 </template>
-
 <script lang="ts">
 import { defineComponent } from 'vue';
-import {
-  IonPage,
-  IonHeader,
-  IonToolbar,
-  IonTitle,
-  IonContent,
-  IonFab,
-  IonFabButton,
-  IonIcon,
-  IonGrid,
-  IonRow,
-  IonCol,
-  IonImg
-} from '@ionic/vue';
-import { camera } from "ionicons/icons";
-import { userPhotoGallery } from "@/composable/userPhotoGallery";
+import { actionSheetController, IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonFab, IonFabButton, IonIcon, IonGrid, IonRow, IonCol, IonImg } from '@ionic/vue';
+import { camera, trash, close } from "ionicons/icons";
+import { userPhotoGallery, UserPhoto } from "@/composable/userPhotoGallery";
+
 export default defineComponent({
   name: 'Tab1Page',
-  components: {
-    IonHeader,
-    IonToolbar,
-    IonTitle,
-    IonContent,
-    IonPage,
-    IonFab,
-    IonFabButton,
-    IonIcon,
-    IonGrid,
-    IonRow,
-    IonCol,
-    IonImg
-  },
+  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonFab, IonFabButton, IonIcon, IonGrid, IonRow, IonCol, IonImg },
   setup() {
-    const { takePhoto, photos } = userPhotoGallery();
+    const { takePhoto, deletePhoto, photos } = userPhotoGallery();
+    const showActionSheet = async (photo: UserPhoto) => {
+      const actionSheet = await actionSheetController.create({
+        header: 'Photos',
+        buttons: [
+          {
+            text: 'Excluir',
+            role: 'destructive',
+            icon: trash,
+            handler: () => { deletePhoto(photo);},
+          },
+          {
+            text: 'Cancelar',
+            icon: close,
+            role: 'cancel',
+            handler: () => {/* Nenhuma ação quando cancelar*/}, 
+          },
+        ],
+      });
+      await actionSheet.present();
+    };
     return {
       camera,
+      trash,
+      close,
       takePhoto,
-      photos
+      photos,
+      showActionSheet
     }
-
   },
 });
 </script>
